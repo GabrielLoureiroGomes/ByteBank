142fafbbfb4f9ba1d81d453a4a1c80fd3ba28a5f
Gabriel Loureiro <loureirobiel@gmail.com>
2021-02-23 19:31:57 -0300

    implementing null safety

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,10 @@
-fun main() {
+import model.Endereco
 
+fun main() {
+    var enderecoNulo: Endereco? = Endereco(logradouro = "Rua Jorge Pilli Netto")
+    enderecoNulo?.let { rua ->
+        println(rua.logradouro)
+        println(rua.logradouro.length)
+    }
 }
 
